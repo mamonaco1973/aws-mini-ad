6abdf71b8ae78dc34325a8bdbc8696462caa653a
Mike Monaco <mamonaco1973@gmail.com>
2025-08-12 15:23:25 -0400

    fix HTTPS port

--- 01-directory/security.tf
+++ 01-directory/security.tf
@@ -79,7 +79,7 @@ resource "aws_security_group" "ad_sg" {
 
   ingress {
     from_port   = 443
-    to_port     = 4
+    to_port     = 443
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
