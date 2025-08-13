1e08a60e81ad8e1d61af0e42ce6ef3e653005a88
Mike Monaco <mamonaco1973@gmail.com>
2025-08-13 11:22:15 -0400

    remove port 80 from security group

--- 01-directory/security.tf
+++ 01-directory/security.tf
@@ -83,16 +83,7 @@ resource "aws_security_group" "ad_sg" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
-  # HTTP for domain status
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
+  
   # LDAP over SSL
   ingress {
     from_port   = 636
