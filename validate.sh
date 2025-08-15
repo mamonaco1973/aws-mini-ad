f4c6ece646e35a6d4e9af3d52f98eba4f1fd9092
Mike Monaco <mamonaco1973@gmail.com>
2025-08-15 15:24:29 -0400

    add validate.sh

---
+++ validate.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Set your region if needed
+AWS_REGION="us-east-1"
+
+windows_dns=$(aws ec2 describe-instances \
+  --filters "Name=tag:Name,Values=windows-ad-instance" \
+  --query 'Reservations[].Instances[].PublicDnsName' \
+  --output text)
+
+linux_dns=$(aws ec2 describe-instances \
+  --filters "Name=tag:Name,Values=linux-ad-instance" \
+  --query 'Reservations[].Instances[].PublicDnsName' \
+  --output text)
+
+echo "NOTE: Windows Instance: $windows_dns"
+echo "NOTE: Linux Instance: $linux_dns"
